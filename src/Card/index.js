460b8d4c2bb68228e45770aa7f7947c347b152dc
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2021-06-16 15:18:04 -0700

    Added a little padding around the 'show more' button

--- src/Card/index.js
+++ src/Card/index.js
@@ -31,7 +31,7 @@ function getSelectedText() {
     var text = "";
     if (window.getSelection) {
         text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
+    } else if (document.selection && document.selection.type !== "Control") {
         text = document.selection.createRange().text;
     }
     return text;
